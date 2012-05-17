32bb918daa7dade5ccb114e622d5126aa0e39351
Salah Alkawari <salkawari@yahoo.com>
2012-05-18 01:43:45 +0200

    added listener, so that when you click on the graphic, it reruns

--- Animation2/gen/com/example/animation2/R.java
+++ Animation2/gen/com/example/animation2/R.java
@@ -14,11 +14,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int chair_1=0x7f020000;
-        public static final int chair_2=0x7f020001;
-        public static final int chair_3=0x7f020002;
-        public static final int chair_4=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
+        public static final int ic_launcher=0x7f020000;
+        public static final int slide1=0x7f020001;
+        public static final int slide2=0x7f020002;
+        public static final int slide3=0x7f020003;
+        public static final int slide4=0x7f020004;
+        public static final int slide5=0x7f020005;
     }
     public static final class id {
         public static final int stickman=0x7f060000;
@@ -27,7 +28,8 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
+        public static final int app_name=0x7f050002;
+        public static final int desc=0x7f050001;
         public static final int hello=0x7f050000;
     }
 }
--- Animation2/src/com/example/animation2/Animation2Activity.java
+++ Animation2/src/com/example/animation2/Animation2Activity.java
@@ -2,33 +2,132 @@ package com.example.animation2;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.os.Bundle;
 
 public class Animation2Activity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-           final ImageView animImage = (ImageView) 
-           findViewById(R.id.stickman);
-           final AnimationDrawable animDrawable = 
-           (AnimationDrawable) animImage.getDrawable();
-           animImage.post(
-        	        new Runnable() {
-        	          public void run() { 
-        	            animDrawable.start();
-        	          }
-        	        }
-        	      );
-        	    }
-        	  }    
-/*           
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	// constants..
+	private static final String TAG =Animation2Activity.class.getSimpleName();
+	private static final boolean IS_DEBUG = true;
+
+	// these are used in the different methods..
+	ImageView animImageStickman;
+	AnimationDrawable animDrawableStickman;	
+
+	// this is the lister that will be stuck onto animImageStickman..
+	OnClickListener OnClickStickman;
+
+	//useful logging method..
+	private void logDebug(String log_message) {
+		if (IS_DEBUG) { Log.d(TAG, log_message); } 
+	}
+
+	// this method causes the animation to run..
+	private void runAnimationViaPost() {
+		boolean bool_result = false;
+		//		animImageStickman.postInvalidate();
+		//		animDrawableStickman = (AnimationDrawable) animImageStickman.getDrawable();
+		bool_result = animImageStickman.post(
+				new Runnable() {
+					public void run() { 
+						try{
+							logDebug("starting animation!");
+							//animImageStickman.postInvalidate();
+							//animDrawableStickman = (AnimationDrawable) animImageStickman.getDrawable();
+							//animDrawableStickman.invalidateSelf();
+							animImageStickman.setImageDrawable(animDrawableStickman);
+							animDrawableStickman.start();
+							// this is the reset bit..
+							animDrawableStickman.setVisible(true, true);
+							
+						}catch(Throwable tr) { Log.e(TAG, "Error in runAnimationViaPost! ",tr);}
+					}
+				}
+				);		
+		logDebug("bool_result="+bool_result);
+	}
+	//Called when the activity is first created. 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		try {						
+			super.onCreate(savedInstanceState);	
+			setContentView(R.layout.main);
+
+			// make an image view for r.id.stickman..			
+			animImageStickman = (ImageView) 
+					findViewById(R.id.stickman);
+
+			animDrawableStickman = (AnimationDrawable) animImageStickman.getDrawable();
+
+			// we run the animation (using post)..
+			runAnimationViaPost();			
+
+			// we set a listener onto this image and override the onclick behaviour..
+			animImageStickman.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {					
+					runAnimationViaPost();		
+				}
+			}
+
+					);
+
+			////////////////////////////////////////////////////////////////////
+			// this is the first way of doing it from the animate book..
+			//animDrawableStickman = (AnimationDrawable) animImageStickman.getDrawable();
+			//animImageStickman.post(
+			//	        new Runnable() {
+			//	          public void run() { 
+			//	        	  logDebug("starting animation!");
+			//	        	  animDrawableStickman.start();
+			//	          }
+			//	        }
+			//	      );			
+			////////////////////////////////////////////////////////////////////
+			////////////////////////////////////////////////////////////////////
+			// this is one way of doing it - from andriod developer.com	
+			////////////////////////////////////////////////////////////////////
+			//import android.view.MotionEvent;
+			//import android.view.View;
+			//animImageStickman.setBackgroundResource(R.anim.stickman);
+			//
+			//animDrawableStickman = (AnimationDrawable) animImageStickman.getBackground();
+			//
+			//// set the listener onto the view..
+			//animImageStickman.setOnClickListener( new OnClickListener() {
+			//
+			//	@Override
+			//	public void onClick(View v) { 
+			//		logDebug("onClick..!");
+			//		animDrawableStickman.start(); }
+			//}
+			//);
+			//////////////////////////////////////////////////////////////////////
+		}catch(Throwable tr) { Log.e(TAG, "Error in onCreate! ",tr);}
+	}
+	//@Override
+	//public void onStart() {
+	//	try {
+	//		logDebug("in onStart");
+	//
+	//		super.onStart();	
+	//		//setContentView(R.layout.main);
+	//
+	//		// make an image view for r.id.stickman..
+	//		animImageStickman = (ImageView) 
+	//				findViewById(R.id.stickman);
+	//
+	//		
+	//		runAnimationViaPost();
+	//	}catch(Throwable tr) { Log.e(TAG, "Error in onStart! ",tr);}	
+	//}
 }
-           
-           */
+//addListenerOnStickman();
+
+
+
+
